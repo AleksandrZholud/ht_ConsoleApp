6a90119e3c6a51ad52dff000a57d5a88c79ef7b7
OZholud <ozholud@eva.com>
2022-05-29 00:09:05 +0300

    init Commit

---
+++ src/main/java/hometask/ht_botscrew/HtBotsCrewApplication.java
@@ -0,0 +1,13 @@
+package hometask.ht_botscrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtBotsCrewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtBotsCrewApplication.class, args);
+    }
+
+}
