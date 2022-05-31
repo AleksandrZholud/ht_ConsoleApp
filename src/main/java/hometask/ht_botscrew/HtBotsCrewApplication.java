9e09f8bfba7ae301765a7c4eb7f25bdd4b1b1d90
OZholud <ozholud@eva.com>
2022-05-31 13:42:52 +0300

    hello world

--- src/main/java/hometask/ht_botscrew/HtBotsCrewApplication.java
+++ src/main/java/hometask/ht_botscrew/HtBotsCrewApplication.java
@@ -7,8 +7,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HtBotsCrewApplication{
+public class HtBotsCrewApplication implements CommandLineRunner {
+
     public static void main(String[] args) {
+        LOG.info("STARTING THE APPLICATION");
         SpringApplication.run(HtBotsCrewApplication.class, args);
+        LOG.info("APPLICATION FINISHED");
     }
+    private static Logger LOG = LoggerFactory
+            .getLogger(HtBotsCrewApplication.class);
+
+    @Override
+    public void run(String... args) throws Exception {LOG.info("EXECUTING : command line runner");
+
+        for (int i = 0; i < args.length; ++i) {
+            LOG.info("args[{}]: {}", i, args[i]);
+        }
+        System.out.println("\n\nHello, World!)\n\n");
+    }
+
 }
